11e28f91621b6073cbea9440a47180289dea18e8
CChuancey <37216768+CChuancey@users.noreply.github.com>
2026-02-22 11:37:59 +0800

    Update Footer.vue
    更新项目信息

--- assets/Footer.vue
+++ assets/Footer.vue
@@ -20,18 +20,18 @@
     </div>
 
     <div class="project-links">
-      <strong>项目:</strong><a href="https://github.com/willow-god/FlareDrive-R2" target="_blank"
+      <strong>项目:</strong><a href="https://github.com/CChuancey/FlareDrive-R2.git" target="_blank"
         rel="noopener noreferrer">FlareDrive-R2</a>
-      <strong>作者:</strong><a href="https://github.com/willow-god" target="_blank"
+      <strong>作者:</strong><a href="https://github.com/CChuancey" target="_blank"
         rel="noopener noreferrer">LiuShen</a>
-      <strong>初始项目:</strong><a href="https://github.com/ljxi/Cloudflare-R2-oss" target="_blank"
+      <strong>初始项目:</strong><a href="https://github.com/willow-god/FlareDrive-R2" target="_blank"
         rel="noopener noreferrer">Cloudflare-R2-OSS</a>
     </div>
 
     <div class="powered">Powered by <a href="https://workers.cloudflare.com/" target="_blank"
         rel="noopener noreferrer">Cloudflare Workers</a></div>
 
-    <div style="margin:10px 0;">© 2025 FlareDrive. All rights reserved.</div>
+    <div style="margin:10px 0;">© 2026 FlareDrive. All rights reserved.</div>
   </footer>
 </template>
 
